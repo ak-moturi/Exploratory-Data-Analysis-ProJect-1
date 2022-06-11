19fc957901041753fad0fd69df40d72f6b9f42f9
akmoturi <75719513+ak-moturi@users.noreply.github.com>
2022-06-11 08:46:50 +0300

    plot2.R

--- plot2.R
+++ plot2.R
@@ -1,9 +1,13 @@
+# Read data into R and subset the dates of interest
 filepath <- "./data/household_power_consumption.txt"
-powerdata <- read.table(filepath, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-powersubset<- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+powerdata <- read.table(filepath, header=TRUE, sep=";", stringsAsFactors=FALSE)
+powersubset<-subset(powerdata,powerdata$Date=="1/2/2007" | powerdata$Date =="2/2/2007")
 
+# Transform the date and time variable into objects of type Date and POSIXlt
 datetime <- strptime(paste(powersubset$Date, powersubset$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globaLpower <- as.numeric(powersubset$Global_active_power)
+globalpower <- as.numeric(powersubset$Global_active_power)
+
+#Set graphic device and plot
 png("plot2.png", width=480, height=480)
 plot(datetime, globalpower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
